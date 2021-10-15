59e95f6b20ce14aa879c1240c06e0ee83eaa1511
Sakib0194 <mohammad.sakib0194@gmail.com>
2021-10-15 20:54:01 +0600

    added hovering point value + chart value, tooltip + colors, legends, upper axis

--- Chart_Design.py
+++ Chart_Design.py
@@ -2,8 +2,7 @@ from PyQt5.QtWidgets import QWidget
 from PyQt5.QtChart import QChart, QChartView, QLineSeries, QValueAxis, QCategoryAxis
 from PyQt5.QtGui import QPainter, QPen
 from PyQt5.QtCore import Qt
-
-
+import sys, os
 from pyqtgraph.Qt import QtGui as graphGui
 from pyqtgraph.Qt import QtCore as graphCore
 import pyqtgraph as pg
@@ -17,23 +16,27 @@ class create_chart(QWidget):
         self.plot = pg.PlotWidget()
         self.p1 = ''
         self.p2 = pg.ViewBox()
+        self.all_data = {}
+        self.kpi_data = {}
+        self.dates = []
+        
 
     def remvove_widget(self):
-        
+        #remove existing widget so the new one can be placed
         if self.cu_widget != '':
             self.ui.verticalLayout_3.removeWidget(self.cu_widget)
         
     def updateViews(self):
-        ## view has resized; update auxiliary views to match
+        #don't know what it does. taken from pyqtgraph examples
         self.p2.setGeometry(self.p1.vb.sceneBoundingRect())
-        
-        ## need to re-update linked axes since this was called
-        ## incorrectly while views had different shapes.
-        ## (probably this should be handled in ViewBox.resizeEvent)
-        self.p2.linkedViewChanged(self.p1.vb, self.p2.YAxis)
+        self.p2.linkedViewChanged(self.p1.vb, self.p2.XAxis)
 
     def create_chart(self, data, kpi_data):
         data = dict(sorted(data.items()))
+        self.all_data = data
+        self.kpi_data = kpi_data
+        self.dates = []
+
         self.plot = pg.PlotWidget()
         self.plot.showGrid(x=True, y=True)
         self.plot.setBackground('w')
@@ -41,13 +44,11 @@ class create_chart(QWidget):
         xax_2 = self.plot.getAxis('top')
 
         self.p1 = self.plot.plotItem
-        self.p1.setLabels(top='axis 1')
         self.p2 = pg.ViewBox()
         self.p1.showAxis('top')
         self.p1.scene().addItem(self.p2)
         self.p1.getAxis('top').linkToView(self.p2)
         self.p2.setXLink(self.p1)
-        self.p1.getAxis('top').setLabel('axis2', color='#0000ff')
 
         
         axis_x_all = []
@@ -59,18 +60,18 @@ class create_chart(QWidget):
         cu_next = 0
         next_x_label = 0
 
-        if len(data) > 12:
-            next_x_label = int(len(data) / 12)
+        if len(data) > 10:
+            next_x_label = int(len(data) / 10)
 
         for i in data:
-
+            self.dates.append(i)
             axis_x_all.append(num)
             axis_y_all.append(data[i])
 
             if cu_next != 0:
                 cu_next -= 1
             else:
-                new_lab = f'{(str(i))[6:]}-{(str(i))[4:6]}' 
+                new_lab = f'{str(i)[6:8]}-{str(i)[4:6]}' 
                 x_labels[0].append((num, new_lab))
                 cu_next = next_x_label
 
@@ -91,20 +92,17 @@ class create_chart(QWidget):
         xax_2.setTicks(x_labels)
         self.plot.plot(axis_x_all, axis_y_all, name="Total Message", pen='b')
         self.plot.plot(axis_x_kpi, axis_y_kpi, name='KPI Message', pen='g')
-        #self.p1.plot(axis_x_all)
-        #self.p1.plot(axis_x_all)
-        #self.p2.addItem(pg.PlotCurveItem(axis_x_all))
-        #self.p2.addItem(pg.PlotCurveItem(axis_x_all))
         self.cu_widget = self.plot
         self.ui.verticalLayout_3.addWidget(self.plot)
 
     def draw_cursor(self):
         #cross hair
-        self.vLine = pg.InfiniteLine(angle=90, movable=False, pen=pg.mkPen('k', width=1), label='{value:0.1f}',
+        self.vLine = pg.InfiniteLine(angle=90, movable=False, pen=pg.mkPen('k', width=1), label=None,
                                         labelOpts={'position':0.98, 'color': (200,0,0), 'movable': True, 'fill': (0, 0, 200, 100)})
         self.hLine = pg.InfiniteLine(angle=0, movable=False, pen=pg.mkPen('k', width=1), label='{value:0.1f}',
                                         labelOpts={'position':0.98, 'color': (200,0,0), 'movable': True, 'fill': (0, 0, 200, 100)})
         self.plot.addItem(self.hLine, ignoreBounds=True)
+        self.plot.addItem(self.vLine, ignoreBounds=True)
         self.vb = self.plot.plotItem.vb
     
         #set mouse event
@@ -120,6 +118,23 @@ class create_chart(QWidget):
                     mousePoint = self.vb.mapSceneToView(pos)
                     self.vLine.setPos(mousePoint.x())
                     self.hLine.setPos(mousePoint.y())
+                    full_text = ''
+                    try:
+                        mousePoint = int(mousePoint.x())
+                        date_time = self.dates[mousePoint]
+                        readable_date = f'{str(date_time)[6:8]}-{str(date_time)[4:6]}-{str(date_time)[:4]}'
+                        full_text = f"<span style=\"color:black;font-size:10pt\">Date: {readable_date}</span><br>"
+                        full_text += f"<span style=\"color:blue;font-size:10pt\">Message Count: {self.all_data[date_time]}</span><br>"
+                        if date_time in self.kpi_data:
+                            full_text += f"<span style=\"color:green;font-size:10pt\">KPI Count: {self.kpi_data[date_time]}</span>"
+                        else:
+                            full_text += f"<span style=\"color:green;font-size:10pt\">KPI Count: 0</span>"
+                        #full_text = 
+                        #full_text += f"<span style=\"color:green;font-size:15pt\">All Message Count: {self.all_data[date_time]}</span>"
+                        self.plot.setToolTip(full_text)
+                    except Exception as e:
+                        self.plot.setToolTip(full_text)
+                        #print(e)
 
             return graphGui.QWidget.eventFilter(self, source, event)
         except Exception as e:
